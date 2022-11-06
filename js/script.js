ec15dfe3b29d58315b8259f3eb5126b518537db3
Jennifer Arboleda <arboleda.jen@gmail.com>
2022-11-05 21:23:24 -0500

    created 2 fetch API calls. One to get latitude/longitude from user input of city. Second uses these coordinates to get weather.

--- js/script.js
+++ js/script.js
@@ -4,22 +4,29 @@ var apiKey = "69ee81bf4e18fd7668c841c7d1ea128b";
 var inputCity = document.querySelector('#inputCity');
 var inputState = document.querySelector('#inputState');
 var inputCountry = document.querySelector('#inputCountry');
-var button = document.querySelector('#searchButton');
+var searchButton = document.querySelector('#searchButton');
 
-//save user input values to localstorage
-function saveToLocalStorage () {
-    localStorage.setItem('city', inputCity.value);
-    localStorage.setItem('state', inputState.value);
-    localStorage.setItem('country', inputCountry.value);
-}
-button.addEventListener("click", saveToLocalStorage); //calls function to save to local storage
 
-// fetch("http://api.openweathermap.org/data/2.5/forecast?lat=41.878113&lon=-87.629799&appid=" + key)
-//     .then(response => response.json())
-//     .then (data => console.log(data))
- 
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
 
-//Get latitude and longitude
-// fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + inputCity.value + inputState.value + inputCountry.value + '&limit=3&appid=' + apiKey)
-//     .then(response => response.json())
-//     .then (data => console.log(data));
+    var userInput =  {
+        city: inputCity.value,
+        state: inputState.value,
+        country: inputCountry.value,
+    };
+
+    //fetch openweatherapi to get latitude and longitude of city
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userInput.city},${userInput.state},${userInput.country}US&limit=1&appid=` + apiKey)
+        .then(response => response.json())
+        .then (data => {
+            var latValue = data[0]['lat'];
+            var lonValue = data[0]['lon'];
+
+    //fetch and get weather data via longitude and latitude
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latValue}&lon=${lonValue}&appid=` + apiKey)
+        .then(response => response.json())
+        .then (data => console.log(data))
+
+    });
+});
